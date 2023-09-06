473bdf55da850b51a9ef5d3a727e1d4c81710312
zunicorn <10700158-zunicorn@users.noreply.gitlab.com>
2023-09-06 19:57:11 -0300

    Cleaning up

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,7 +1,6 @@
 return {
     {"hrsh7th/nvim-cmp",
         version = false, -- last release is way too old
-        event = "InsertEnter",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
@@ -10,7 +9,86 @@ return {
             "jalvesaq/cmp-nvim-r"
         },
         config = function()
-            require("r_constants")
+
+            local cmp = require'cmp'
+
+            cmp.setup({
+                snippet = {
+                    -- REQUIRED - you must specify a snippet engine
+                    expand = function(args)
+                        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    end,
+                },
+                window = {
+                    --completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    -- { name = 'ultisnips' }, -- For ultisnips users.
+                    -- { name = 'snippy' }, -- For snippy users.
+                }, {
+                        { name = 'buffer' },
+                    }),
+
+                formatting = {
+                    format = function(entry, vim_item)
+                        --vim_item.kind = "kind"
+                        vim_item.menu = "menu"
+                        vim_item.menu = ({
+                            path = "[PATH]",
+                            cmp_nvim_r = "[R]",
+                            r_constants = "[R]",
+                            nvim_lsp = "[LSP]",
+                            buffer = "[BUFFER]",
+                            luasnip = "[LSNIP]"
+                        })[entry.source.name]
+                        return vim_item
+                    end,
+                },
+            })
+
+            -- Set configuration for specific filetype.
+            cmp.setup.filetype('r', {
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'cmp_nvim_r' },
+                }, {
+                        { name = 'buffer' },
+                    })
+            })
+
+            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
+            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+            --cmp.setup.cmdline(':', {
+            --    mapping = cmp.mapping.preset.cmdline(),
+            --    sources = cmp.config.sources({
+            --        { name = 'path' }
+            --    }, {
+            --            { name = 'cmdline' }
+            --        })
+            --})
         end
     }
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,75 +2,72 @@ return {
     -- Configs for Nvim LSP client
     {"neovim/nvim-lspconfig",
         config = function()
-            local lspconfig = require('lspconfig')
-            local lsp_defaults = lspconfig.util.default_config
-            local configs = require('lspconfig/configs')
-            
-            lsp_defaults.capabilities = vim.tbl_deep_extend(
-              'force',
-              lsp_defaults.capabilities,
-              require('cmp_nvim_lsp').default_capabilities()
-            )
-            
-            vim.diagnostic.config({
-              virtual_text = false,
-              signs = true,
-              underline = true,
-              update_in_insert = false,
-              severity_sort = false,
-            })
-            
-            local signs = { Error = "⮾", Warn = "⚠", Hint = "⚑", Info = "🛈" }
-            for type, icon in pairs(signs) do
-              local hl = "DiagnosticSign" .. type
-              vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-            end
+            -- Setup language servers.
+            --local lspconfig = require('lspconfig')
+            -- lspconfig.pyright.setup {}
+            -- lspconfig.tsserver.setup {}
+            -- lspconfig.rust_analyzer.setup {
+            --     -- Server-specific settings. See `:help lspconfig-setup`
+            --     settings = {
+            --         ['rust-analyzer'] = {},
+            --     },
+            -- }
+
+            -- Global mappings.
+            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+            -- Use LspAttach autocommand to only map the following keys
+            -- after the language server attaches to the current buffer
             vim.api.nvim_create_autocmd('LspAttach', {
-              desc = 'LSP actions',
-              callback = function()
-                local bufmap = function(mode, lhs, rhs)
-                  local opts = {buffer = true}
-                  vim.keymap.set(mode, lhs, rhs, opts)
-                end
-            
-                -- Displays hover information about the symbol under the cursor
-                bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-            
-                -- Jump to the definition
-                bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-            
-                -- Jump to declaration
-                bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-            
-                -- Lists all the implementations for the symbol under the cursor
-                bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-            
-                -- Jumps to the definition of the type symbol
-                bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-            
-                -- Lists all the references 
-                bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-            
-                -- Displays a function's signature information
-                bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-            
-                -- Renames all references to the symbol under the cursor
-                bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-            
-                -- Selects a code action available at the current cursor position
-                bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-                bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
-            
-                -- Show diagnostics in a floating window
-                bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-            
-                -- Move to the previous diagnostic
-                bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-            
-                -- Move to the next diagnostic
-                bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-              end
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wl', function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<space>f', function()
+                        vim.lsp.buf.format { async = true }
+                    end, opts)
+                    vim.diagnostic.config({virtual_text = false})
+
+                    -- Dialog borders
+                    local _border = "single"
+                    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+                        vim.lsp.handlers.hover, {
+                            border = _border
+                        }
+                    )
+                    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+                        vim.lsp.handlers.signature_help, {
+                            border = _border
+                        }
+                    )
+                    vim.diagnostic.config{
+                        float={border=_border}
+                    }
+                    require('lspconfig.ui.windows').default_options = {
+                        border = _border
+                    }
+                end,
             })
-        end,
+        end
     }
 }
