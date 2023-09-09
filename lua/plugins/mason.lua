9d468114953c9e48bed0bb2dc6f922a866ae0dfd
zunicorn <10700158-zunicorn@users.noreply.gitlab.com>
2023-09-08 21:26:49 -0300

    Adding lua_lsp settings
    Commented out for now

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,7 +6,7 @@ return {
         config = function()
             require("mason").setup()
             require("mason-lspconfig").setup()
-            
+
             require("mason-lspconfig").setup_handlers {
                 -- The first entry (without a key) will be the default handler
                 -- and will be called for each installed server that doesn't have
@@ -14,15 +14,19 @@ return {
                 function (server_name) -- default handler (optional)
                     require("lspconfig")[server_name].setup {}
                 end,
-                --["r_language_server"] = function()
-                --    require("lspconfig").r_language_server.setup {
-                --        server_capabilities = {
-                --            completionProvider = false,
-                --            completionItemResolve = false
-                --        }
-                --    }
-                --end
+
+                -- ["lua_ls"] = function ()
+                --     require("lspconfig").lua_ls.setup {
+                --         settings = {
+                --             Lua = {
+                --                 diagnostics = {
+                --                     enable = false,
+                --                 }
+                --             }
+                --         }
+                --     }
+                -- end,
             }
         end
     }
-} 
+}
