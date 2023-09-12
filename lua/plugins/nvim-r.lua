41a39a4ab19abd95ae0168b9f32b7dd99567af87
Lucas <lucas_git.nvgw4@slmail.me>
2023-09-11 21:07:02 -0300

    Changed Rout colors
    Default colors were burning my eyes

--- lua/plugins/nvim-r.lua
+++ lua/plugins/nvim-r.lua
@@ -1,6 +1,6 @@
 return {
     {"jalvesaq/Nvim-R",
-        config = function()        
+        config = function()
             vim.g.R_assign_map = '<M-->'
             vim.g.R_csv_app = "terminal:vd"
             vim.g.r_indent_align_args = 1
@@ -15,6 +15,7 @@ return {
             vim.g.R_objbr_opendf = 0
             vim.g.R_objbr_openlist = 0
             vim.g.R_min_editor_width = 100
+            vim.g.rout_follow_colorscheme = 1
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = {"r", "rmd", "rnoweb"},
                 callback = function(args)
