dccaade17a74c13e2c02699bca88c3fa0087eabe
Lucas <lucas_git.nvgw4@slmail.me>
2023-09-16 15:37:49 -0300

    File browser extension for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,17 @@
 return {
     {"nvim-telescope/telescope.nvim",
         branch = "0.1.x",
-        dependencies = {"nvim-lua/plenary.nvim"},
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-file-browser.nvim"
+        },
         config = function ()
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>fe", "<cmd>Telescope file_browser<CR>", {})
             require("telescope").setup {
                 defaults = {
                     mappings = {
@@ -22,10 +26,23 @@ return {
                 pickers = {
                     find_files = {disable_devicons = true},
                     live_grep = {disable_devicons = true},
-                    buffers = {disable_devicons = true},
+                    buffers = {
+                        disable_devicons = true,
+                        sort_mru = true
+                    },
                     help_tags = {disable_devicons = true},
+                },
+                extensions = {
+                    file_browser = {
+                        dir_icon = "🗀",
+                        git_status = false,
+                        select_buffer = true,
+                        display_stat = false,
+                        grouped = false
+                    }
                 }
             }
+            require("telescope").load_extension "file_browser"
         end
     }
 }
