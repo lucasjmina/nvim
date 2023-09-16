0bacd8829ed09b114f7b1e93dfc69472c51e884c
Lucas <lucas_git.nvgw4@slmail.me>
2023-09-15 21:09:55 -0300

    Disabling neovim-tree
    Using netrw for now, it's more than enough right now. Might give neo-tree a try someday

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,6 +3,7 @@ return {
     {'nvim-tree/nvim-tree.lua',
         version = '*',
         lazy = false,
+        enabled = false,
         dependencies = {
             "nvim-tree/nvim-web-devicons",
         },
