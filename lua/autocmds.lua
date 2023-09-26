75732a96e9bcb9919513c5da101d42b484438022
Lucas <lucas_git.nvgw4@slmail.me>
2023-09-25 21:30:26 -0300

    Autocmd to disable linenumbers in the terminal

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,3 +4,9 @@ vim.api.nvim_create_autocmd("BufNewFile", {
     pattern = {"*.R"},
     command = (skeletons .. "skel.R | goto 99")
 })
+
+-- Disable linenumbers in terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+    pattern = "*",
+    command = ("setlocal nonumber norelativenumber")
+})
