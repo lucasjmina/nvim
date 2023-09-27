fdc7e98b6b032acb937e464f415b623fb55e9b7d
Lucas <lucas_git.nvgw4@slmail.me>
2023-09-27 15:18:50 -0300

    Snippets for R

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,7 +1,8 @@
 return {
     "L3MON4D3/LuaSnip",
-    dependencies = {
-        "saadparwaiz1/cmp_luasnip"
-    },
+    config = function ()
+        local snippets = vim.fn.stdpath("config") .. "/snippets"
+        require("luasnip.loaders.from_vscode").lazy_load { paths = snippets }
+    end
 }
 
