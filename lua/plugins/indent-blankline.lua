c621d9985d1566518c8faa86864c15773c6f41b5
Lucas <lucas_git.nvgw4@slmail.me>
2023-10-01 19:14:28 -0300

    Updates
    indent-blankline changed to version 3, migrated config

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,7 +4,8 @@ return {
             --vim.opt.list = true
             --vim.opt.listchars:append "space:⋅"
             --vim.opt.listchars:append "eol:↴"
-            require("indent_blankline").setup {
+            require("ibl").setup {
+                indent = {char = "▏"},
                 filetype_exclude = {
                     "lspinfo",
                     "packer",
