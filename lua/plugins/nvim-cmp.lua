632217521315aadd33cdc299a9cd93e2ba1afb35
Lucas <lucas_git.nvgw4@slmail.me>
2023-10-12 17:17:54 -0300

    Commands completion

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -6,7 +6,8 @@ return {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "saadparwaiz1/cmp_luasnip",
-            "jalvesaq/cmp-nvim-r"
+            "jalvesaq/cmp-nvim-r",
+            "hrsh7th/cmp-cmdline"
         },
         config = function()
 
@@ -106,14 +107,14 @@ return {
             })
 
             -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-            --cmp.setup.cmdline(':', {
-            --    mapping = cmp.mapping.preset.cmdline(),
-            --    sources = cmp.config.sources({
-            --        { name = 'path' }
-            --    }, {
-            --            { name = 'cmdline' }
-            --        })
-            --})
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                        { name = 'cmdline' }
+                    })
+            })
         end
     }
 }
