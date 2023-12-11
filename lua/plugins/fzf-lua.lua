84e240afdf94a469e90d9f7fa97ee3cd9d5d7639
Lucas <lucas_git.nvgw4@slmail.me>
2023-12-11 17:46:31 -0300

    Added fzf-lua keymaps
    Keymaps for LSP diagnostics, code actions, document symbols and references

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -8,7 +8,14 @@ return {
         vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", {}),
         vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", {}),
         vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", {}),
-        vim.keymap.set("n", "<leader>fl", "<cmd>FzfLua lines<CR>", {})
+        vim.keymap.set("n", "<leader>fl", "<cmd>FzfLua lines<CR>", {}),
+        vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua lsp_document_diagnostics<CR>", {}),
+        vim.keymap.set("n", "<leader>fa", "<cmd>FzfLua lsp_code_actions<CR>", {}),
+        vim.keymap.set("n", "<leader>fs", "<cmd>FzfLua lsp_document_symbols<CR>", {}),
+        vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua lsp_references<CR>", {})
+
+
+
     })
   end
 }
