410efc689ad7221cc70d5fbda1c85a34e0962e87
Lucas <lucas_git.nvgw4@slmail.me>
2023-12-17 21:35:11 -0300

    Ctrl+y for opening fzf-lua selected files in buffers

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,8 +3,30 @@ return {
   -- optional for icon support
   -- dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({
+        -- TODO: There has to be a better way to do this instead of repeating it likes this.
+        grep = {
+            actions = {
+                ["ctrl-y"]      = function(selected)
+                    for _, e in ipairs(selected) do
+                        local file = require'fzf-lua'.path.entry_to_file(e)
+                        vim.cmd("badd " .. file.path)
+                    end
+                end,
+            },
+        },
+        files = {
+            actions = {
+                ["ctrl-y"]      = function(selected)
+                    for _, e in ipairs(selected) do
+                        local file = require'fzf-lua'.path.entry_to_file(e)
+                        vim.cmd("badd " .. file.path)
+                    end
+                end,
+            },
+        },
         vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", {}),
         vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", {}),
         vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", {}),
