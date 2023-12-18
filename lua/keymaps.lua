ecdfd9aeb9b6eeac37f18b1df26e8cbf7aa25f43
Lucas <lucas_git.nvgw4@slmail.me>
2023-12-17 21:34:07 -0300

    Ctrl+Space for exiting insert mode in terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<Leader>s',":exec 'norm '.(90 - strlen(getline('.'))).'A-'<CR>")
 vim.keymap.set("n", "<Leader>e", "<cmd>Explore<CR>")
+vim.keymap.set('t', '<C-space>', [[<C-\><C-n>]])
