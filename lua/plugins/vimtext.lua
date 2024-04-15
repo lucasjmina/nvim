6cdcfead0baf1245fef13375718eb3bc848882c0
LucasJM <lucas_git.nvgw4@slmail.me>
2024-04-15 17:42:49 -0300

    Added vimtex plugin

---
+++ lua/plugins/vimtext.lua
@@ -0,0 +1,9 @@
+return {
+    "lervag/vimtex",
+    init = function()
+        -- Use init for configuration, don't use the more common "config".
+        vim.g.vimtex_compiler_latexmk = {
+            continuous = 0
+        }
+    end
+}
