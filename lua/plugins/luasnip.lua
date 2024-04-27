63629b3ba4e4e04284f19f18da6aafb340fe27bb
LucasJM <lucas_git.nvgw4@slmail.me>
2024-04-27 20:14:03 -0300

    Cleaned up unused function

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,7 +2,6 @@ return {
     "L3MON4D3/LuaSnip",
     dependencies = { "rafamadriz/friendly-snippets" },
     config = function ()
-        local snippets = vim.fn.stdpath("config") .. "/snippets"
         require("luasnip.loaders.from_vscode").lazy_load()
     end
 }
