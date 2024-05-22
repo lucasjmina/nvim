5f3793396202ac0e85c55021b986aed08f4eca8b
LucasJM <lucas_git.nvgw4@slmail.me>
2024-05-22 17:29:26 -0300

    Left capabilities configurarion to R options

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -23,16 +23,6 @@ return {
             lspconfig.bashls.setup {}
             lspconfig.r_language_server.setup {
                 cmd = {require("mason-registry").get_package("r-languageserver"):get_install_path() .. "/bin/r-languageserver"},
-                settings = {
-                    r = {
-                        lsp = {
-                            server_capabilities = {
-                                completionProvider = false,
-                                completionItemResolve = false
-                            }
-                        }
-                    }
-                },
                 root_dir = function(fname)
                     return lspconfig.util.root_pattern("*.Rproj", ".git")(fname) or vim.loop.os_homedir()
                 end,
