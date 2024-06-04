1256e7f999067b9dcacbcfdb541cbd9160228c86
LucasJM <lucas_git.nvgw4@slmail.me>
2024-06-04 20:53:55 -0300

    Disabled nvim-r highlight, using tree-sitter's

--- lua/plugins/nvim-r.lua
+++ lua/plugins/nvim-r.lua
@@ -6,16 +6,15 @@ return {
             vim.g.r_indent_align_args = 0
             vim.g.r_indent_ess_comments = 0
             vim.g.r_indent_ess_compatible = 0
-            vim.g.R_hi_fun_paren = 1
             vim.g.R_objbr_auto_start = 1
             vim.g.R_rconsole_width = 0
-            --vim.g.R_fun_data_1 = {'select', 'rename', 'mutate', 'filter'}
-            --vim.g.R_fun_data_2 = "{'ggplot': ['aes'], 'with': ['lm', 'glm', 'lmer']}"
             vim.g.R_rconsole_height = 10
             vim.g.R_objbr_opendf = 0
             vim.g.R_objbr_openlist = 0
             vim.g.R_min_editor_width = 100
             vim.g.rout_follow_colorscheme = 1
+            vim.g.R_hl_term = 0
+            vim.g.R_hi_fun = 0
             vim.g.R_nvim_wd = 1
             vim.g.R_pdfviewer = "evince"
         end,
