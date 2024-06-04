bedfe1d3053c3698901750d9022dc04ddf786302
LucasJM <lucas_git.nvgw4@slmail.me>
2024-06-04 20:52:45 -0300

    Removed some stuff that is defined elsewhere now

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -15,14 +15,7 @@ return {
 
         require('mini.pick').setup()
 
-        require('mini.bufremove').setup({
-            vim.api.nvim_set_keymap("n", "<leader>bd", "", {
-                noremap = true,
-                callback = function()
-                    MiniBufremove.delete()
-                end
-            })
-        })
+        require('mini.bufremove').setup()
 
         local ai = require('mini.ai')
         ai.setup({
@@ -32,40 +25,10 @@ return {
             },
         })
 
-        require('mini.files').setup({
-            -- Do not show icons
-            -- content = { prefix = function() end },
-            vim.api.nvim_set_keymap("n", "<leader>fe", "", {
-                noremap = true,
-                callback = function()
-                    MiniFiles.open(vim.api.nvim_buf_get_name(0))
-                end,
-            })
-        })
+        require('mini.files').setup()
 
         require('mini.indentscope').setup({ symbol = "│" })
 
         require('mini.statusline').setup()
-
-        require("mini.base16").setup({
-            palette = {
-                base00 = "#fafafa",
-                base01 = "#f0f0f1",
-                base02 = "#e5e5e6",
-                base03 = "#a0a1a7",
-                base04 = "#696c77",
-                base05 = "#383a42",
-                base06 = "#202227",
-                base07 = "#090a0b",
-                base08 = "#ca1243",
-                base09 = "#d75f00",
-                base0A = "#c18401",
-                base0B = "#50a14f",
-                base0C = "#0184bc",
-                base0D = "#4078f2",
-                base0E = "#a626a4",
-                base0F = "#986801"
-            }
-        })
     end
 }
