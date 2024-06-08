1bebf7b7bd9a0bd4d9ac36a866bd9c3e4a5c80f6
LucasJM <lucas_git.nvgw4@slmail.me>
2024-06-08 20:43:06 -0300

    Changes to keymaps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,53 +1,58 @@
 return {
-    "hrsh7th/nvim-cmp",
-    version = false, -- last release is way too old
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-        local cmp = require 'cmp'
+  "hrsh7th/nvim-cmp",
+  version = false, -- last release is way too old
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+  },
+  config = function()
+    local cmp = require 'cmp'
 
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-                    elseif require("luasnip").locally_jumpable(1) then
-                        require("luasnip").jump(1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-                    elseif require("luasnip").locally_jumpable(-1) then
-                        require("luasnip").jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" })
-            }),
-            snippet = {
-                -- REQUIRED - you must specify a snippet engine
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
-                end,
-            },
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-                { name = 'path',    option = { get_cwd = function() return vim.fn.getcwd() end, } }, -- use nvim cwd as root
-                { name = 'buffer',  keyword_length = 3 },
-            }),
-        })
-    end
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-c>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<esc>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.abort()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if require("luasnip").locally_jumpable(1) then
+            require("luasnip").jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if require("luasnip").locally_jumpable(-1) then
+            require("luasnip").jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" })
+      }),
+      snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path',    option = { get_cwd = function() return vim.fn.getcwd() end, } }, -- use nvim cwd as root
+        { name = 'buffer',  keyword_length = 3 },
+      }),
+    })
+  end
 }
