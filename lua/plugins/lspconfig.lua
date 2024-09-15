f2ca65233667114ce9440b7b70646f5a7a676818
LucasJM <lucas_git.nvgw4@slmail.me>
2024-09-15 01:48:01 -0300

    Use R LSP installed from CRAN Function to get the path of r-languageserver installed with Mason no longer needed.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,13 +3,6 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
         local lspconfig = require('lspconfig')
-        local path = function(server_name)
-            return { require("mason-registry").get_package(server_name):get_install_path() .. "/bin/" .. server_name }
-        end
-        -- Set neovim cwd with lsp root
-        -- local custom_attach = function(client)
-        --     vim.fn.chdir(client.config.root_dir)
-        -- end
         local diagnostic_opts = {
             -- Show gutter sings
             signs = {
@@ -34,7 +27,6 @@ return {
         lspconfig.bashls.setup {}
 
         lspconfig.r_language_server.setup {
-            cmd = path("r-languageserver"),
             root_dir = function(fname)
                 return lspconfig.util.root_pattern("*.Rproj", ".git")(fname) or vim.loop.os_homedir()
             end,
