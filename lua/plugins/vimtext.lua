10d85d0c3cbcf89129155a1592d4cd1155fb4efe
LucasJM <lucas_git.nvgw4@slmail.me>
2024-12-21 21:31:37 -0300

    Disabled vimtex
    Just some plugin cleanup

--- lua/plugins/vimtext.lua
+++ lua/plugins/vimtext.lua
@@ -1,5 +1,6 @@
 return {
     "lervag/vimtex",
+    enabled = false,
     init = function()
         -- Use init for configuration, don't use the more common "config".
         vim.g.vimtex_compiler_latexmk = {
